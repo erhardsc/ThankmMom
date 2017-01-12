fb8cb7aca3913c52235cf2b4e567e3fb59134858
Sumner <serhard2556@gmail.com>
2017-01-11 19:00:04 -0700

    batteryService

--- app/src/main/java/com/sumnererhard/thanksmom/MainActivity.java
+++ app/src/main/java/com/sumnererhard/thanksmom/MainActivity.java
@@ -1,84 +1,27 @@
 package com.sumnererhard.thanksmom;
 
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.BatteryManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
-
-import java.io.File;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
-
-
+    private batteryService myService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        detectCharge();
 
-       // instance = this;
-//        try {
-//            clearApplicationData();
-//        } catch (Exception e){
-//            Toast.makeText(getApplicationContext(), "Sorry, not enough cache to delete right now", Toast.LENGTH_LONG).show();
-//        }
+        if (myService == null){
+             //start the service
+             //int oldRuntime = myService.getSystemTime();
+             Intent intent = new Intent(this, batteryService.class);
+             startService(intent);
+         }
 
-    }
-
-    public void detectCharge() {
-
-        //create an IntentFilter object that matches battery change action
-        IntentFilter filter=new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-
-        //register broadcast receiver to receive a sticky intent
-        Intent intent=this.registerReceiver(null, filter);
-
-        //get battery status from the intent
-        int status=intent.getIntExtra(BatteryManager.EXTRA_STATUS,-1);
-        if(status==BatteryManager.BATTERY_STATUS_CHARGING)
-            Toast.makeText(this,"The battery is charging.", Toast.LENGTH_LONG).show();
-
-        else if (status==BatteryManager.BATTERY_STATUS_DISCHARGING)
-            Toast.makeText(this,"The battery is discharging.", Toast.LENGTH_LONG).show();
-
-        else if(status==BatteryManager.BATTERY_STATUS_FULL)
-            Toast.makeText(this,"The battery is full.", Toast.LENGTH_LONG).show();
-    }
-
-    public void clearApplicationData() {
-        File cacheDirectory = getCacheDir();
-        File applicationDirectory = new File(cacheDirectory.getParent());
-        if (applicationDirectory.exists()) {
-            String[] fileNames = applicationDirectory.list();
-            for (String fileName : fileNames) {
-                if (!fileName.equals("lib")) {
-                    deleteFile(new File(applicationDirectory, fileName));
-                }
-            }
-            Toast.makeText(getApplicationContext(), "Deleting Application Cache", Toast.LENGTH_LONG).show();
-            Toast.makeText(getApplicationContext(), "Thanks Mom", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    public static boolean deleteFile(File file) {
-        boolean deletedAll = false;
-        if (file != null) {
-            if (file.isDirectory()) {
-                String[] children = file.list();
-                for (int i = 0; i < children.length; i++) {
-                    deletedAll = deleteFile(new File(file, children[i])) && deletedAll;
-                }
-            } else {
-                deletedAll = file.delete();
-            }
-        }
-
-        return deletedAll;
     }
 }
+
+
--- app/src/main/java/com/sumnererhard/thanksmom/batteryService.java
+++ app/src/main/java/com/sumnererhard/thanksmom/batteryService.java
@@ -1,8 +1,130 @@
 package com.sumnererhard.thanksmom;
 
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.Months;
+import org.joda.time.MutableDateTime;
+import org.joda.time.Weeks;
+
+import java.lang.reflect.Method;
+
+import static android.content.Intent.ACTION_POWER_CONNECTED;
+import static android.content.Intent.ACTION_POWER_DISCONNECTED;
+import static org.joda.time.Weeks.weeksBetween;
+
 /**
  * Created by sumnererhard on 1/6/17.
  */
 
-public class batteryService {
+public class batteryService extends Service {
+
+    private MainActivity main;
+
+    private BroadcastReceiver mBatteryStatus = new BroadcastReceiver() {
+        public int runTime;
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ACTION_POWER_CONNECTED) && getSystemTime() - runTime == 7) {
+                Toast.makeText(context, "Juicin'", Toast.LENGTH_SHORT).show();
+                clearApplicationData();
+                int total = getSystemTime() - runTime;
+                Log.d("New Run time", "value: " + total);
+                runTime = getSystemTime();
+            } else {
+
+                intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED);
+                Toast.makeText(context, "Not Juicin'", Toast.LENGTH_SHORT).show();
+                runTime = getSystemTime();
+                Log.d("New Run time", "value: " + runTime);
+            }
+        }
+    };
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        final IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_POWER_CONNECTED);
+        filter.addAction(ACTION_POWER_DISCONNECTED);
+        this.registerReceiver(mBatteryStatus, filter);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    public int getSystemTime() {
+        MutableDateTime epoch = new MutableDateTime();
+        epoch.setDate(0);
+        DateTime now = new DateTime();
+
+        Days days = Days.daysBetween(epoch, now);
+        Weeks weeks = weeksBetween(epoch, now);
+        Months months = Months.monthsBetween(epoch, now);
+
+        SharedPreferences.Editor editor = getSharedPreferences("prefs_general.xml", MODE_PRIVATE).edit();
+
+        return days.getDays();
+    }
+
+    public void clearApplicationData() {
+        PackageManager pm = getPackageManager();
+        Method[] methods = pm.getClass().getDeclaredMethods();
+        for (Method m : methods) {
+            if (m.getName().equals("freeStorage")){
+                try{
+                    Log.i("Method Package", m.toGenericString());
+                } catch (Exception e){
+
+                }
+                break;
+            }
+        }
+
+//        try {
+//            File cacheDirectory = getCacheDir();
+//            File applicationDirectory = new File(cacheDirectory.getParent());
+//            if (applicationDirectory.exists()) {
+//                String[] fileNames = applicationDirectory.list();
+//                for (String fileName : fileNames) {
+//                    if (!fileName.equals("lib")) {
+//                        deleteFile(new File(applicationDirectory, fileName));
+//                    }
+//                }
+//                Toast.makeText(getApplicationContext(), "Deleting Application Cache", Toast.LENGTH_LONG).show();
+//                Toast.makeText(getApplicationContext(), "Thanks Mom", Toast.LENGTH_LONG).show();
+//            }
+//        }catch(Exception e){
+//            Toast.makeText(getApplicationContext(), "Sorry, not enough cache to delete right now", Toast.LENGTH_LONG).show();
+//        }
+    }
+
+//    public static boolean deleteFile(File file) {
+//        boolean deletedAll = false;
+//        if (file != null) {
+//            if (file.isDirectory()) {
+//                String[] children = file.list();
+//                for (int i = 0; i < children.length; i++) {
+//                    deletedAll = deleteFile(new File(file, children[i])) && deletedAll;
+//                }
+//            } else {
+//                deletedAll = file.delete();
+//            }
+//        }
+//
+//        return deletedAll;
+//    }
 }

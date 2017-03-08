4ab09783a313e8ee4d3a9e288bed1e6ae2354b96
Sumner <serhard2556@gmail.com>
2017-03-07 18:13:22 -0700

    Added Package Observer and worked on Caching bug

--- app/src/main/java/com/sumnererhard/thanksmom/tab2Info.java
+++ app/src/main/java/com/sumnererhard/thanksmom/tab2Info.java
@@ -1,77 +1,130 @@
 package com.sumnererhard.thanksmom;
 
+import android.content.pm.IPackageDataObserver;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
-import java.io.File;
-import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Created by sumnererhard on 1/16/17.
  */
 public class Tab2Info extends Fragment {
 
+    private static final long CACHE_APP = Long.MAX_VALUE;
+
+    private CachePackageDataObserver mClearCacheObserver;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.tab2_info_fragment, container, false);
 
-        Button btnTraverse = (Button)rootView.findViewById(R.id.btn_traverse);
+        Button btnTraverse = (Button) rootView.findViewById(R.id.btn_traverse);
 
-        btnTraverse.setOnClickListener( new View.OnClickListener() {
+        btnTraverse.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-
-                Toast.makeText(getContext(), "Btn Click", Toast.LENGTH_SHORT).show();
-                Log.d("BTN CLICK", "vWoooooow: ");
-                try {
-                    Runtime.getRuntime().exec("su");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                File dir = new File(".");
-
-                traverse(dir);
-
+                clearCache();
             }
         });
 
         return rootView;
     }
 
-
-    public void traverse (File dir) {
-        if (dir.exists()) {
-            File[] parentDir = dir.listFiles();
-
-            for (int i = 0; i < parentDir.length; ++i) {
-
-                File file = parentDir[i];
-
-                if (file.isDirectory() && file.getName().equals("data")) { /// FIGURE OUT HOW TO LIST THE DIRECTORIES IN THE ANDROID ECOSYSTEM
-
-                    Log.d("Directories", "Directory value: " + file);
-                    File[] dataDir = file.listFiles();
-
-                    for (int j = 0; j < dataDir.length; ++j){
-                        File dataFile = dataDir[j];
-                        Log.d("FILES", "value: " + dataFile);
-                    }
-
-                } else {
-                    // do something here with the file
-
-                }
-            }
+    void clearCache()
+    {
+        if (mClearCacheObserver == null)
+        {
+            mClearCacheObserver=new CachePackageDataObserver();
         }
-    }
+
+        PackageManager mPM=getContext().getPackageManager();
+
+        @SuppressWarnings("rawtypes")
+        final Class[] classes= { Long.TYPE, IPackageDataObserver.class };
+
+        Long localLong=Long.valueOf(CACHE_APP);
+
+        try
+        {
+            Method localMethod=
+                    mPM.getClass().getMethod("freeStorageAndNotify", classes);
+
+      /*
+       * Start of inner try-catch block
+       */
+            try
+            {
+                localMethod.invoke(mPM, localLong, mClearCacheObserver);
+            }
+            catch (IllegalArgumentException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch (IllegalAccessException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch (InvocationTargetException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+      /*
+       * End of inner try-catch block
+       */
+        }
+        catch (NoSuchMethodException e1)
+        {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+    }//End of clearCache() method
+
+    private class CachePackageDataObserver extends IPackageDataObserver.Stub
+    {
+        public void onRemoveCompleted(String packageName, boolean succeeded)
+        {
+
+        }//End of onRemoveCompleted() method
+    }//End of CachePackageDataObserver instance inner class
+
+
+
+//    public void traverse (File dir) {
+//        if (dir.exists()) {
+//            File[] parentDir = dir.listFiles();
+//
+//            for (int i = 0; i < parentDir.length; ++i) {
+//
+//                File file = parentDir[i];
+//
+//                if (file.isDirectory() ) { /// FIGURE OUT HOW TO LIST THE DIRECTORIES IN THE ANDROID ECOSYSTEM
+//
+//                    Log.d("Directories", "Directory value: " + file);
+//                    File[] dataDir = file.listFiles();
+////
+////                    for (int j = 0; j < dataDir.length; ++j){
+////                        File dataFile = dataDir[j];
+////                        Log.d("FILES", "value: " + dataFile);
+////                    }
+//
+//                } else {
+//                    // do something here with the file
+//
+//                }
+//            }
+//        }
+ //   }
 
 }
